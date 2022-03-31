f319fc2decae363624c91a7e6f85917ea728628d
unknown <hafizzwijaya@gmail.com>
2022-03-31 15:34:17 +0700

    modified package

--- application/views/Package/V_package.php
+++ application/views/Package/V_package.php
@@ -107,25 +107,25 @@
       ================================================== -->
         <!-- Wrap the rest of the page in another container to center all the content. -->
 
-        <div class="container marketing mt-5">
+        <div class="container marketing" style="padding-top: 7rem;">
 
             <!-- Three columns of text below the carousel -->
             <div class="row">
                 <div class="col-lg-4">
-                    <img class="img-fluid img-thumnail" src="assets\image\decoration\Paket 1\WhatsApp Image 2022-03-11 at 15.18.36 (1).jpeg" alt="Generic placeholder image" width="140" height="140">
-                    <h2>Paket 1</h2>
+                    <img class="rounded" src="assets\image\decoration\Paket 1\WhatsApp Image 2022-03-11 at 15.18.36 (1).jpeg" alt="Generic placeholder image" width="140" height="140">
+                    <h2>Wedding Package</h2>
                     <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
                 </div>
                 <div class="col-lg-4">
                     <img class="rounded" src="assets\image\ringsRose.jpg" alt="Generic placeholder image" width="140" height="140">
-                    <h2>Paket 2</h2>
+                    <h2>Engagement Package</h2>
                     <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
                 </div>
                 <div class="col-lg-4">
                     <img class="rounded" src="assets\image\small\DSC03334.jpg" alt="Generic placeholder image" width="140" height="140">
-                    <h2>Paket 3</h2>
+                    <h2>Birthday Package</h2>
                     <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
                 </div>
