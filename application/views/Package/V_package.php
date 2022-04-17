b5b53fc5add666821e5d6a76bfbd6d97fa492f1d
Hafizh <apidsanjaya@gmail.com>
2022-04-17 17:18:19 +0700

    modify -f acer

--- application/views/Package/V_package.php
+++ application/views/Package/V_package.php
@@ -10,7 +10,7 @@
     <!-- MODIFY -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-    <meta name="HandheldFriendy" content="true">
+    <meta name="HandheldFriendly" content="true">
 
 
     <!-- Bootstrap CSS -->
